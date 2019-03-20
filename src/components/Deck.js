5de9d0421abacdca15bcd0b5cc632ff3fe24d637
deepakcodefreak <deepak19.engg@gmail.com>
2019-03-20 20:41:20 +0530

    card returning to default condition on lossing hold

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -16,10 +16,16 @@ class Deck extends React.Component{
             onPanResponderMove:(event,gesture)=>{
                 position.setValue({x:gesture.dx,y:gesture.dy})
             },
-            onPanResponderRelease:()=>{}
+            onPanResponderRelease:()=>{
+                this.resetPosition()
+            }
         })
 
         this.state = {panResponder,position}
+    }
+
+    resetPosition(){
+        Animated.spring(this.state.position,{toValue:{x:0,y:0}}).start();
     }
 
     getCardStyle(){
